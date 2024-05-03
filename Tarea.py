1751d78b5ab02ed75d5ccfbaa828252dff28c00c
Manuel Garcia Romo <mg.romo24@gmail.com>
2024-05-03 10:54:07 +0200

    Clases estado y tarea

--- Tarea.py
+++ Tarea.py
@@ -6,25 +6,34 @@ class EstadoTarea:
     completada = 'Completada'
 
 class Tarea:
-    def __init__(self, id_tarea = int, titulo = str, descripcion = str, estado = str):
+    def __init__(self, id_tarea = int, titulo = str, descripcion = str, estimacion_coste = int):
         self.id_tarea = id_tarea
         self.titulo = titulo
         self.descripcion = descripcion
-        self.estado = estado
+        self.estimacion_coste = estimacion_coste
+        self.estado = EstadoTarea.pendiente
+        self.subtareas = {}
 
     def EditarTarea(self, titulo, descripcion):
         self.titulo = titulo
         self.descripcion = descripcion
 
-    def CambiarEstado(self, estado):
-        self.estado = estado
+    def AsignarUsuario(self, usuario):
+        self.usuario = usuario
+        self.estado = EstadoTarea.en_curso
+
+    def AnyadirSubtarea(self, subt):
+        self.subtareas[subt] = EstadoTarea.en_curso
+
+    def CambiarEstado(self, subt, estado):
+        self.subtareas[subt] = estado
+        for i in range(len(self.subtareas)):
+            if self.subtareas[i] != EstadoTarea.completada:
+                pass
+        self.estado = EstadoTarea.completada
 
     def __str__(self):
         return f'Id: {self.id_tarea}, {self.titulo}: {self.descripcion}. {self.estado}.'
 
-tarea1 = Tarea(24, 'Tarea 1', 'Realizar la Tarea1', EstadoTarea.pendiente)
-tarea1.CambiarEstado(EstadoTarea.en_curso)
-tarea1.EditarTarea('Tarea2', 'Sí')
-print(tarea1)
 
 
